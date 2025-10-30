3fabea354fcccdacec71911376d7301f38583e70
OGBLYNZ <87534619+Lithexf@users.noreply.github.com>
2025-10-30 15:52:18 +0100

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beadando_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //hello world
+        }
+    }
+}
