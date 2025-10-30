aa29f8bcf0682eeafa1e504f82316264dda1db62
OGBLYNZ <87534619+Lithexf@users.noreply.github.com>
2025-10-30 15:37:06 +0100

    main

---
+++ szenzorhalo_szim/szenzorhalo_szim/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szenzorhalo_szim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
