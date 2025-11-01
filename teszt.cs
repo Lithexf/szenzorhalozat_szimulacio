8284c77b2ab759c00aa49e2f1886b2a8f9b853aa
Adenis01 <balazsudvardy1@gmail.com>
2025-11-01 14:00:01 +0100

    teszt2

--- teszt.cs
+++ teszt.cs
@@ -10,10 +10,10 @@ namespace szenzorhalo_szim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ez egy teszt push");
+            Console.WriteLine("Ez egy teszt push2");
 
             //teszt push
-            //"All right, I've been thinking. When life gives you lemons? Don't make lemonade. Make life take the lemons back! Get mad! 'I don't want your damn lemons! What am I supposed to do with these?'"
+            //balazs
         }
     }
 }
