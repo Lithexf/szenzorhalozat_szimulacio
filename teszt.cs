134faced4da4add6d5308163bcba1bef29a47fc6
Adenis01 <balazsudvardy1@gmail.com>
2025-11-01 13:36:26 +0100

    teszt balazs

---
+++ teszt.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szenzorhalo_szim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez egy teszt push");
+
+            //teszt push
+            //"All right, I've been thinking. When life gives you lemons? Don't make lemonade. Make life take the lemons back! Get mad! 'I don't want your damn lemons! What am I supposed to do with these?'"
+        }
+    }
+}
