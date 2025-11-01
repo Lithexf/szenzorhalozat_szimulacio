8dad46d31e210a0a7ef01aa39e968f89e68d8c08
csurkemartin0730 <csurkemartin0730@gmail.com>
2025-11-01 11:13:10 +0100

    teszt commit

--- szenzorhalo_szim/szenzorhalo_szim/Program.cs
+++ szenzorhalo_szim/szenzorhalo_szim/Program.cs
@@ -13,6 +13,7 @@ namespace szenzorhalo_szim
             Console.WriteLine("Hello World");
 
             //teszt push
+            //"All right, I've been thinking. When life gives you lemons? Don't make lemonade. Make life take the lemons back! Get mad! 'I don't want your damn lemons! What am I supposed to do with these?'"
         }
     }
 }
