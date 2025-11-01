943920da8f547ace0a484c6b8f316b9a0a6b3689
OGBLYNZ <87534619+Lithexf@users.noreply.github.com>
2025-11-01 13:59:08 +0100

    sew

--- program.cs
+++ program.cs
@@ -11,7 +11,7 @@ namespace beadando_v2
         static void Main(string[] args)
         {
           //hello world
-          console.WriteLine("Hello, World!");
+          cwrite("Hello, World!");
         }
     }
 }
