cd82393bfad189133b55771e3f8c2b0c12bf6888
OGBLYNZ <87534619+Lithexf@users.noreply.github.com>
2025-11-27 23:34:19 +0100

    Add files via upload

---
+++ MeresKonyvtar/SzenzorSzimulacio/SzenzorSzimulacio.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using LiteDB;
+using MeresKonyvtar;
+
+namespace SzenzorSzimulacio
+{
+    // Sallai Martin --
+    class SzenzorSzimulacio
+    {
+        public delegate void AradasEsemenykezelo(string uzenet);
+        public static event AradasEsemenykezelo OnAradas;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szenzorhálózati Szimuláció");
+            Console.WriteLine("Vízszint mérő program");
+            Console.CursorVisible = false;
+
+            OnAradas += FigyelmeztetesKiiro;
+            SzenzorMeresGeneralas generator = new SzenzorMeresGeneralas();
+            List<NapiMeres> hetiAdatok = new List<NapiMeres>();
+            string[] napok = { "Hétfő", 
+                                "Kedd", 
+                                "Szerda", 
+                                "Csütörtök", 
+                                "Péntek", 
+                                "Szombat", 
+                                "Vasárnap" };
+
+            //kötelező áradás nap kiválasztása 
+            //esemény kezelés, esemény = áradás
+            Random rnd = new Random();
+            int katasztrofaEsemeny = rnd.Next(0, napok.Length);
+
+            
+            for (int i = 0; i < napok.Length; i++)
+            {
+                string nap = napok[i];
+                Console.WriteLine($"\n\nJelenlegi Nap: {nap}");
+
+                //megnézzük hogy garantáltan áradós nap van e
+                bool maiNapLegyenAradas = (i == katasztrofaEsemeny);
+                List<MeresAdat> aktualisMeresek = generator.Meresgeneralas(maiNapLegyenAradas); //mérés lista generálása
+
+
+                int kritikusErtekekSzama = aktualisMeresek.Count(x => x.VizszintCm > 400); //ellenőrzés kritikus értékekre
+                double atlagSzint = aktualisMeresek.Average(x => x.VizszintCm);
+                string ertekekAdat = string.Join(", ", aktualisMeresek.Select(x => x.VizszintCm));
+
+                Console.WriteLine($"A mai mérések: {ertekekAdat}"); //mérés kiiratása
+
+                string statusz;
+                if (kritikusErtekekSzama >= 3) //kondició
+                {
+                    statusz = "Áradás! ";
+                    OnAradas?.Invoke($"Áradás Történt! (Kritikus Értékek: {kritikusErtekekSzama}db!)");
+                }
+                else if (kritikusErtekekSzama >= 2)  //kis figyelmeztetés
+                {
+                    statusz = "Magas Vízszint Veszélye";
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    SargaKiiro($"Státusz: {statusz} (Kritikus Értékek: {kritikusErtekekSzama}db)");
+                }
+
+                else //normál állapot
+                {
+                    statusz = "Átlagos vízszint:";
+                    Console.WriteLine($"Státusz: {statusz} (Átlag: {atlagSzint:F1} cm)");
+                }
+                
+                hetiAdatok.Add(new NapiMeres
+                {
+                    Nap = nap,
+                    Meresek = aktualisMeresek,
+                    Statusz = statusz
+                });
+                //adatok
+            }
+            mentes(hetiAdatok);
+
+
+            Console.WriteLine("\nProgram vége, Gomb nyomásra kiléphet!");
+            Console.ReadKey();
+        }
+        static void FigyelmeztetesKiiro(string uzenet)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write(uzenet);
+            //visszaállítjuk a színt
+            Console.ResetColor();
+            Console.WriteLine("");
+        }
+// Csürke Martin ---
+        static void SargaKiiro(string uzenet) 
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write(uzenet);
+            //visszaállítjuk a színt
+            Console.ResetColor();
+            Console.WriteLine("");
+        }
+        static void mentes(List<NapiMeres> adatok) {
+            //JSON fájlba mentés
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(adatok, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText("meresek.json", jsonString);
+            
+            // elmentjük a méréseket szöveges fájlban
+            using (StreamWriter sw = new StreamWriter("adatbazis.txt"))
+            {
+                sw.WriteLine("Nap;SzenzorID;Vizszint;Statusz");
+                foreach (var napAdat in adatok)
+                {
+                    foreach (var meres in napAdat.Meresek)
+                    {
+                        sw.WriteLine($"{napAdat.Nap};{meres.SzenzorID};{meres.VizszintCm};{napAdat.Statusz}");
+                    }
+                }
+            }
+
+            //elmentjük adatbázisban
+            var db = new LiteDatabase("Filename=adatok.db; Connection=shared;");
+            var meresek = db.GetCollection<NapiMeres>();
+            foreach (var meres in adatok)
+            {
+                meresek.Insert(meres);
+            }
+        }
+
+    }
+
+    class NapiMeres
+    {
+        public string Nap { get; set; }
+        public List<MeresAdat> Meresek { get; set; }
+        public string Statusz { get; set; }
+    }
+}
+// Csürke Martin ---
